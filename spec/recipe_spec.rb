cfff0eb87d6bd3f0dea42ce28de0988b2b822c6d
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-08-02 09:43:16 -0600

    Test: Add test for adding ingredients required

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -20,4 +20,25 @@ describe Recipe do
       expect(@recipe1.ingredients_required).to eq({})
     end
   end
+
+  describe 'adding ingredients' do
+    before do
+      @recipe1 = Recipe.new("Mac and Cheese")
+      @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+      @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    end
+
+    xit 'can add ingredients' do
+      @recipe1.add_ingredient(@ingredient1, 2)
+      @recipe1.add_ingredient(@ingredient1, 4)
+      @recipe1.add_ingredient(@ingredient2, 8)
+
+      expected_hash = {
+        @ingredient1 => 6,
+        @ingredient2 => 8
+      }
+
+      expect(@recipe1.ingredients_required).to eq(expected_hash)
+    end
+  end
 end

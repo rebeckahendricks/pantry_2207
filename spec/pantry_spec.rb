09027048cce3e65a53208f7d371dc3ea7498d720
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-08-02 09:20:30 -0600

    Test: Create stock_check and restock tests

--- spec/pantry_spec.rb
+++ spec/pantry_spec.rb
@@ -15,4 +15,27 @@ describe Pantry do
       expect(@pantry.stock).to eq({})
     end
   end
+
+  describe 'stock' do
+    before do
+      @pantry = Pantry.new
+      @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+      @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+    end
+
+    xit 'can check the stock of a specific ingredient' do
+      expect(@pantry.stock_check(@ingredient1)).to eq(0)
+    end
+
+    xit 'can restock an ingredient' do
+      @pantry.restock(@ingredient1, 5)
+      @pantry.restock(@ingredient1, 10)
+
+      expect(@pantry.stock_check(@ingredient1)).to eq(15)
+
+      @pantry.restock(@ingredient2, 7)
+
+      expect(@pantry.stock_check(@ingredient2)).to eq(7)
+    end
+  end
 end

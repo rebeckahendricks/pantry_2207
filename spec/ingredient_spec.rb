25299d9b4b6af17c4ab7e8c260f9828adc03d9d5
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-08-02 09:09:43 -0600

    Feat: Create ingredient class and attributes

--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -7,19 +7,19 @@ describe Ingredient do
       @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
     end
 
-    xit 'exists' do
+    it 'exists' do
       expect(@ingredient1).to be_instance_of(Ingredient)
     end
 
-    xit 'has a name' do
+    it 'has a name' do
       expect(@ingredient1.name).to eq("Cheese")
     end
 
-    xit 'has a unit' do
+    it 'has a unit' do
       expect(@ingredient1.unit).to eq("oz")
     end
 
-    xit 'has calories' do
+    it 'has calories' do
       expect(@ingredient1.calories).to eq(50)
     end
   end

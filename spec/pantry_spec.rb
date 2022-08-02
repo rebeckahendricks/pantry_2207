89b225b2e3c977b71127fe72b850b46fe944a813
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-08-02 10:28:06 -0600

    Test: Create test for enough_ingredients_for method

--- spec/pantry_spec.rb
+++ spec/pantry_spec.rb
@@ -1,4 +1,6 @@
 require './lib/ingredient'
+require './lib/recipe'
+require './lib/cook_book'
 require './lib/pantry'
 
 describe Pantry do
@@ -36,6 +38,42 @@ describe Pantry do
       @pantry.restock(@ingredient2, 7)
 
       expect(@pantry.stock_check(@ingredient2)).to eq(7)
+    end
+  end
+
+  describe 'checking ingredients' do
+    before do
+      @pantry = Pantry.new
+      @cookbook = CookBook.new
+
+      @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+      @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+      @recipe1 = Recipe.new("Mac and Cheese")
+
+      @recipe1.add_ingredient(@ingredient1, 2)
+      @recipe1.add_ingredient(@ingredient2, 8)
+
+      @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+      @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+      @recipe2 = Recipe.new("Cheese Burger")
+
+      @recipe2.add_ingredient(@ingredient1, 2)
+      @recipe2.add_ingredient(@ingredient3, 4)
+      @recipe2.add_ingredient(@ingredient4, 1)
+
+      @cookbook.add_recipe(@recipe1)
+      @cookbook.add_recipe(@recipe2)
+    end
+
+    xit 'can determine if there are enough ingredients for a recipe' do
+      @pantry.restock(@ingredient1, 5)
+      @pantry.restock(@ingredient1, 10)
+
+      expect(@pantry.enough_ingredients_for?(@recipe1)).to eq(false)
+
+      @pantry.restock(@ingredient2, 1)
+
+      expect(@pantry.enough_ingredients_for?(@recipe1)).to eq(true)
     end
   end
 end

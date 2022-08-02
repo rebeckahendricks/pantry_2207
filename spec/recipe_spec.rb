a26faaa859b52efca89369afb4928b6aed92a02f
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-08-02 10:03:28 -0600

    Test: Add test for total_calories method

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -1,6 +1,7 @@
 require './lib/ingredient'
 require './lib/recipe'
 require './lib/cook_book'
+require './lib/pantry'
 
 describe Recipe do
   describe 'initialize' do
@@ -39,6 +40,29 @@ describe Recipe do
       }
 
       expect(@recipe1.ingredients_required).to eq(expected_hash)
+    end
+  end
+  describe 'counting calories' do
+    before do
+      @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+      @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+      @recipe1 = Recipe.new("Mac and Cheese")
+
+      @recipe1.add_ingredient(@ingredient1, 2)
+      @recipe1.add_ingredient(@ingredient2, 8)
+
+      @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+      @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+      @recipe2 = Recipe.new("Cheese Burger")
+
+      @recipe2.add_ingredient(@ingredient1, 2)
+      @recipe2.add_ingredient(@ingredient3, 4)
+      @recipe2.add_ingredient(@ingredient4, 1)
+    end
+
+    xit 'can add the total calories of all ingredients in a recipe' do
+      expect(@recipe1.total_calories).to eq(440)
+      expect(@recipe2.total_calories).to eq(675)
     end
   end
 end
